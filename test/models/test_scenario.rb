1eb61fd6b66c3fcc352730dfc501c8512323156a
Eliza Brock <eliza@elizabrocksoftware.com>
2015-05-21 15:23:02 -0500

    Refactor Scenario model to use ActiveRecord.

--- test/models/test_scenario.rb
+++ test/models/test_scenario.rb
@@ -2,88 +2,16 @@ require_relative '../test_helper'
 
 describe Scenario do
   describe "#all" do
-    describe "if there are no scenarios in the database" do
-      it "should return an empty array" do
-        assert_equal [], Scenario.all
-      end
-    end
     describe "if there are scenarios" do
       before do
         create_scenario("Bob")
         create_scenario("Sally")
         create_scenario("Amanda")
-      end
-      it "should return an array" do
-        # You don't need to be pedantic like this.
-        # This is just an example to remind you that you can use multiple "its"
-        assert_equal Array, Scenario.all.class
       end
       it "should return the scenarios in alphabetical order" do
         expected = ["Amanda", "Bob", "Sally"]
         actual = Scenario.all.map{ |scenario| scenario.name }
         assert_equal expected, actual
-      end
-      it "populates the returned scenarios' ids" do
-        expected_ids = Database.execute("SELECT id FROM scenarios order by name ASC").map{ |row| row['id'] }
-        actual_ids = Scenario.all.map{ |scenario| scenario.id }
-        assert_equal expected_ids, actual_ids
-      end
-    end
-  end
-
-  describe "#find" do
-    let(:scenario){ Scenario.new("Make pancakes") }
-    before do
-      scenario.save
-    end
-    describe "if there isn't a matching scenario in the database" do
-      it "should return nil" do
-        assert_equal nil, Scenario.find(14)
-      end
-    end
-    describe "if there is a matching scenario in the database" do
-      it "should return the scenario, populated with id and name" do
-        actual = Scenario.find(scenario.id)
-        assert_equal scenario.id, actual.id
-        assert_equal scenario.name, actual.name
-      end
-    end
-  end
-
-  describe "equality" do
-    describe "when the scenario ids are the same" do
-      it "is true" do
-        scenario1 = Scenario.new("foo")
-        scenario1.save
-        scenario2 = Scenario.all.first
-        assert_equal scenario1, scenario2
-      end
-    end
-    describe "when the scenario ids are not the same" do
-      it "is true" do
-        scenario1 = Scenario.new("foo")
-        scenario1.save
-        scenario2 = Scenario.new("foo")
-        scenario2.save
-        assert scenario1 != scenario2
-      end
-    end
-  end
-
-  describe "#count" do
-    describe "if there are no scenarios in the database" do
-      it "should return 0" do
-        assert_equal 0, Scenario.count
-      end
-    end
-    describe "if there are scenarios" do
-      before do
-        create_scenario("Bob")
-        create_scenario("Sally")
-        create_scenario("Amanda")
-      end
-      it "should return the correct count" do
-        assert_equal 3, Scenario.count
       end
     end
   end
@@ -95,52 +23,11 @@ describe Scenario do
     end
   end
 
-  describe ".save" do
-    describe "if the model is valid" do
-      let(:scenario){ Scenario.new("roast a pig") }
-      it "should return true" do
-        assert scenario.save
-      end
-      it "should save the model to the database" do
-        scenario.save
-        assert_equal 1, Scenario.count
-        last_row = Database.execute("SELECT * FROM scenarios")[0]
-        database_name = last_row['name']
-        assert_equal "roast a pig", database_name
-      end
-      it "should populate the model with id from the database" do
-        scenario.save
-        last_row = Database.execute("SELECT * FROM scenarios")[0]
-        database_id = last_row['id']
-        assert_equal database_id, scenario.id
-      end
-    end
-
-    describe "if the model is invalid" do
-      let(:scenario){ Scenario.new("") }
-      it "should return false" do
-        refute scenario.save
-      end
-      it "should not save the model to the database" do
-        scenario.save
-        assert_equal 0, Scenario.count
-      end
-      it "should populate the error messages" do # I have some qualms.
-        scenario.save
-        assert_equal "\"\" is not a valid scenario name.", scenario.errors
-      end
-    end
-  end
-
   describe ".valid?" do
     describe "with valid data" do
       let(:scenario){ Scenario.new("eat corn on the cob") }
       it "returns true" do
         assert scenario.valid?
-      end
-      it "should set errors to nil" do
-        scenario.valid?
-        assert scenario.errors.nil?
       end
     end
     describe "with no name" do
@@ -150,7 +37,7 @@ describe Scenario do
       end
       it "sets the error message" do
         scenario.valid?
-        assert_equal "\"\" is not a valid scenario name.", scenario.errors
+        assert_equal ["Name can't be blank"], scenario.errors.full_messages
       end
     end
     describe "with empty name" do
@@ -160,7 +47,7 @@ describe Scenario do
       end
       it "sets the error message" do
         scenario.valid?
-        assert_equal "\"\" is not a valid scenario name.", scenario.errors
+        assert_equal ["Name can't be blank"], scenario.errors.full_messages
       end
     end
     describe "with a name with no letter characters" do
@@ -170,7 +57,7 @@ describe Scenario do
       end
       it "sets the error message" do
         scenario.valid?
-        assert_equal "\"777\" is not a valid scenario name.", scenario.errors
+        assert_equal ["Name must contain at least 1 letter"], scenario.errors.full_messages
       end
     end
     describe "with a previously invalid name" do
@@ -182,10 +69,6 @@ describe Scenario do
       end
       it "should return true" do
         assert scenario.valid?
-      end
-      it "should not have an error message" do
-        scenario.valid?
-        assert_nil scenario.errors
       end
     end
   end
